bc8eefa192510ca74c481b033d6e644dd004e108
Kevin Ross <kevin.ross@alienfast.com>
2022-06-17 14:41:47 -0500

    Add throttle utility function

---
+++ src/throttle.ts
@@ -0,0 +1,30 @@
+type Throttled<T extends (...args: any) => any> = (...args: Parameters<T>) => ReturnType<T>
+
+/**
+ * Creates a throttled {callback} will be called at most once per {limit} millisonds.
+ *
+ * @param callback - function
+ * @param limit - milliseconds
+ * @return - In limit mode, it will return last result of the {callback}, otherwise it will invoke the {callback}
+ */
+export function throttle<T extends (...args: any) => any>(
+  callback: T,
+  limit: number,
+): Throttled<T> {
+  let inThrottle: boolean
+  let lastResult: ReturnType<T>
+
+  return function (this: any, ...args): ReturnType<T> {
+    if (!inThrottle) {
+      inThrottle = true
+
+      setTimeout(() => (inThrottle = false), limit)
+
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      lastResult = callback.apply(this, args)
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    return lastResult as any
+  }
+}
